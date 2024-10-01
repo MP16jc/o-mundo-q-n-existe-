6e4e7ef1caeea09fa85d7457c2b802466c9f4450
MP16jc <00001114086265sp@al.educacao.sp.gov.br>
2024-10-01 14:19:11 -0300

    Add files via upload

---
+++ o mundo q n existe/script.js
@@ -0,0 +1,7 @@
+const avanca = document.querySelectorAll('.btn-proximo');
+avanca.forEach(button => {
+    button.addEventListener('click', function () {
+        const atual = documente.querySelector('ative');
+        const proximoPasso = 'passo-' + this.getAttribute('date-proximo');
+    })
+})
